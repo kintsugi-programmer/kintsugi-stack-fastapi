185c07e2ee29649615c1bd249fb2e12525242f29
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2026-03-06 19:43:30 +0000

    feat: initialize + /hello-world

---
+++ main.py
@@ -0,0 +1,9 @@
+import uvicorn
+
+def main():
+    print("Hello from kintsugi-stack-fastapi!")
+    uvicorn.run("src.app:application",host="0.0.0.0",port=8000,reload=True)
+
+
+if __name__ == "__main__":
+    main()
